e9948fd5b2b417ef7d1f29dd86e10b788b8423d2
ujval9 <ujvalshah1@gmail.com>
2022-08-14 03:12:18 +0530

    Initial Commit

---
+++ UiWorksDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UiWorksDemo
+//
+//  Created by Ujval Shah on 14/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UiWorksDemo/UiWorksDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UiWorksDemoApp.swift
+//  UiWorksDemo
+//
+//  Created by Ujval Shah on 14/08/22.
+//
+
+import SwiftUI
+
+@main
+struct UiWorksDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

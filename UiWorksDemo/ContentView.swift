18bc5a1d9645cd24bcff2998c1779ac6e8514afb
ujval9 <ujvalshah1@gmail.com>
2022-08-15 05:18:23 +0530

    Added all the elements

--- UiWorksDemo/ContentView.swift
+++ UiWorksDemo/ContentView.swift
@@ -10,41 +10,249 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Color("black")
+            Color("Black")
         .ignoresSafeArea()
-            HStack {
-                Button(
-                    action: {},
-                       label: {
-                           RoundedRectangle(cornerRadius: 15)
-                               .foregroundColor(.white)
-                               .frame(width: 50, height: 50, alignment: .center)
-                               .overlay(
-                                Image("Profilepic").resizable()
-                                    .scaledToFit()
-                                    .frame(width: 40, height: 40)
-                                    .clipped()
-                               )
-                           
-                       }
-                )
-                Button(
-                    action: {},
-                       label: {
-                           RoundedRectangle(cornerRadius: 15)
-                               .fill(Color("Custom"))
-                               .frame(width: 50, height: 50, alignment: .center)
-                               .overlay(
-                                Image(systemName: "bell.badge")
-                                    .renderingMode(.original)
-                                    .font(.largeTitle)
-                                    .foregroundColor(.white)
-                                    
-                               )
+            VStack {
+                HStack {
+                    Button(
+                        action: {},
+                           label: {
+                               RoundedRectangle(cornerRadius: 15)
+                                   .foregroundColor(.white)
+                                   .frame(width: 50, height: 50, alignment: .center)
+                                   .overlay(
+                                    Image("Profilepic").resizable()
+                                        .scaledToFit()
+                                        .frame(width: 40, height: 40)
+                                        .clipped()
+                                   )
                                
-                           
-                       }
-                )
+                           }
+                    )
+                    Button(
+                        action: {},
+                           label: {
+                               RoundedRectangle(cornerRadius: 15)
+                                   .fill(Color("Custom"))
+                                   .frame(width: 50, height: 50, alignment: .center)
+                                   .overlay(
+                                    Image(systemName: "bell.badge")
+                                        .renderingMode(.original)
+                                        .font(.largeTitle)
+                                        .foregroundColor(.white)
+                                        
+                                   )
+                                   
+                               
+                           }
+                    )
+                    
+                }
+                Text("Portfolio balance").foregroundColor(Color.gray)
+                HStack{
+                    Text(verbatim: "\u{00A3} 15.657,34").foregroundColor(Color.white)
+                    Image(systemName: "eye").foregroundColor(Color("light green"))
+                }
+                HStack{
+                    RoundedRectangle(cornerRadius: 40)
+                        .fill(Color("Greenlabel"))
+                        .frame(width: 100, height: 30, alignment: .center)
+                        .overlay(
+                            Text("+12,1%").foregroundColor(Color("light green"))
+//                                .foregroundColor(Color("lightgreen"))
+                                
+                        )
+                    Text("for today").foregroundColor(Color.gray)
+                    
+        
+                }
+                HStack{
+                    Button(
+                        action: {},
+                           label: {
+                               RoundedRectangle(cornerRadius: 35)
+                                   .fill(Color("CardGreen"))
+                                   .frame(width: 200, height: 150, alignment: .center)
+                                   .overlay(
+                                    VStack{
+                                    Image(systemName: "bell.badge")
+                                        .renderingMode(.original)
+                                        .font(.largeTitle)
+                                        .foregroundColor(.white)
+                                        Text("Top up").font(.title2).fontWeight(.semibold).foregroundColor(Color.black
+                                        ).padding()
+                                        
+                                   
+                                    }
+                               )
+                           }
+                    )
+                    Button(
+                        action: {},
+                           label: {
+                               RoundedRectangle(cornerRadius: 35)
+                                   .fill(Color("Custom"))
+                                   .frame(width: 200, height: 150, alignment: .center)
+                                   .overlay(
+                                    VStack{
+                                    Image(systemName: "bell.badge")
+//                                        .renderingMode(.original)
+                                        .font(.largeTitle)
+                                        .foregroundColor(.white)
+                                        Text("Withdraw").font(.title2).fontWeight(.semibold).foregroundColor(Color.white
+                                        ).padding()
+                                        
+                                   
+                                    }
+                               )
+                           }
+                    )
+                }
+                Text("Share Portfolio").foregroundColor(Color.gray)
+                VStack{
+                    Button(
+                        action: {},
+                           label: {
+                               RoundedRectangle(cornerRadius: 25)
+                                   .fill(Color("Apple"))
+                                   .frame(width:400, height: 125, alignment: .center)
+                                   .overlay(
+                                    HStack{
+                                        RoundedRectangle(cornerRadius: 15)
+                                            .fill(Color("applegreen"))
+                                            .frame(width: 75, height: 75, alignment: .center)
+                                            .overlay(
+                                             Image(systemName: "applelogo")
+                                                 .renderingMode(.original)
+                                                 .font(.largeTitle)
+                                                 .foregroundColor(.black)
+                                                 
+                                            )
+                                        VStack{
+                                            HStack{
+                                        Text("Apple")
+                                                    .font(.title2)
+                                                    .foregroundColor(Color.black
+                                        ).padding()
+                                                
+                                                Text(verbatim:"\u{00A3}1.565")
+                                                    .font(.title2)
+                                                    .foregroundColor(Color.black
+                                                ).padding()
+                                                        
+                                            }
+                                            HStack{
+                                        Text("AAPL")
+                                                    
+                                                    .foregroundColor(Color.black
+                                        ).padding()
+                                                
+                                        Text("3.2%")
+                                                  
+                                                    .foregroundColor(Color.black
+                                                ).padding()
+                                                        
+                                            }
+                                        }
+                                    }
+                               )
+                           }
+                    )
+                    Button(
+                        action: {},
+                           label: {
+                               RoundedRectangle(cornerRadius: 25)
+                                   .fill(Color("Meta"))
+                                   .frame(width:400, height: 125, alignment: .center)
+                                   .overlay(
+                                    HStack{
+                                        RoundedRectangle(cornerRadius: 15)
+                                            .fill(Color("MetaColor"))
+                                            .frame(width: 75, height: 75, alignment: .center)
+                                            .overlay(
+                                             Image("MetaImage")
+                                                
+                                                 
+                                            )
+                                        VStack{
+                                            HStack{
+                                        Text("Meta")
+                                                    .font(.title2)
+                                                    .foregroundColor(Color.white
+                                        ).padding()
+                                                
+                                                Text(verbatim: "\u{00A3}1.678")
+                                                    .font(.title2)
+                                                    .foregroundColor(Color.white
+                                                ).padding()
+                                                        
+                                            }
+                                            HStack{
+                                        Text("MTA")
+                                                    
+                                                    .foregroundColor(Color.white
+                                        ).padding()
+                                                
+                                        Text("4.3%")
+                                                  
+                                                    .foregroundColor(Color.white
+                                                ).padding()
+                                                        
+                                            }
+                                        }
+                                    }
+                               )
+                           }
+                    )
+                    Button(
+                        action: {},
+                           label: {
+                               RoundedRectangle(cornerRadius: 25)
+                                   .fill(Color("AmazonColor"))
+                                   .frame(width:400, height: 125, alignment: .center)
+                                   .overlay(
+                                    HStack{
+                                        RoundedRectangle(cornerRadius: 15)
+                                            .fill(Color("Amazon"))
+                                            .frame(width: 75, height: 75, alignment: .center)
+                                            .overlay(
+                                             Image("AmazonLogo")
+                                                .resizable()
+                                                .frame(width: 35, height: 35)
+                                                
+                                                 
+                                            )
+                                        VStack{
+                                            HStack{
+                                        Text("Amazon")
+                                                    .font(.title2)
+                                                    .foregroundColor(Color.black
+                                        ).padding()
+                                                
+                                                Text(verbatim: "\u{00A3}1.678")
+                                                    .font(.title2)
+                                                    .foregroundColor(Color.black
+                                                ).padding()
+                                                        
+                                            }
+                                            HStack{
+                                        Text("AMZ")
+                                                    
+                                                    .foregroundColor(Color.black
+                                        ).padding()
+                                                
+                                        Text("13.27%")
+                                                  
+                                                    .foregroundColor(Color.black
+                                                ).padding()
+                                                        
+                                            }
+                                        }
+                                    }
+                               )
+                           }
+                    )
+                }
             }
         }
         
@@ -54,7 +262,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.dark)
+            
             
             
             
